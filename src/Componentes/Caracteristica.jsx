d3748722aa81e6f3ac3d9ecb743d294c2daed78a
Gonza <gnzx89@gmail.com>
2024-10-18 13:27:25 -0300

    terminar en resolucion grande

--- src/Componentes/Caracteristica.jsx
+++ src/Componentes/Caracteristica.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { FaBuilding } from "react-icons/fa";
+import { IoConstruct } from "react-icons/io5";
+import { FaUserAlt } from "react-icons/fa";
 
-export const Caracteristica = ({icon , title}) => {
+export const Caracteristica = ({title}) => {
+
+  const retonarIcono = ref => {
+    if(ref === 'construccion'){return <FaBuilding className='caracteristica-icono'/>}
+    else if(ref === 'planificacion') {return <IoConstruct className='caracteristica-icono'/>}
+    else if(ref === 'atencion') {return <FaUserAlt className='caracteristica-icono'/>}
+  }
+
   return (
+
     <div className='caracteristica-container'>
-        {icon && <icon className='caracteristica-icon'/>}
+      {retonarIcono(title)}
         <p className='caracteristica-title'>{title}</p>
         <p className='caracteristica-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id pariatur nulla ipsa ipsam eaque commodi officiis</p>
     </div>
--- src/Componentes/Caracteristicas.jsx
+++ src/Componentes/Caracteristicas.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import '../Estilos/Caracteristicas.css';
-import { FaBuilding } from "react-icons/fa";
-import { IoConstruct } from "react-icons/io5";
-import { FaUserAlt } from "react-icons/fa";
 import { Caracteristica } from './Caracteristica';
-
-
-
 
 
 export const Caracteristicas = () => {
@@ -15,9 +9,9 @@ export const Caracteristicas = () => {
         <div className="img-container"></div>
         <div className='listacracteristicas-container'>
         <div className="listacaracteristicas">
-            <Caracteristica icon = {FaBuilding} title = {'construcción'}/>
-            <Caracteristica icon = {IoConstruct} title = {'planificación'}/>
-            <Caracteristica icon = {FaUserAlt} title = {'atención'}/>
+            <Caracteristica title = {'construcción'}/>
+            <Caracteristica title = {'planificación'}/>
+            <Caracteristica title = {'atención'}/>
         </div>
         </div>
         
