5a927b18d1d62d4c0226faf40ae1b0a40f075abf
GonzaS89 <gnzx89@gmail.com>
2024-10-19 17:05:17 -0300

    terminada en teoria

--- src/Componentes/Infocontacto.jsx
+++ src/Componentes/Infocontacto.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { FaPhoneAlt } from "react-icons/fa";
-import { IoLocationSharp } from "react-icons/io5";
-import { FaClock } from "react-icons/fa";
+import { FaCircle } from "react-icons/fa";
 
 export const Infocontacto = (props) => {
 
-  const retonarIconoContacto = ref => {
-    if(ref === 'dir'){return <IoLocationSharp className='icon-contacto'/>}
-    else if(ref === 'tel') {return < FaPhoneAlt className='icon-contacto'/>}
-    else if(ref === 'hora') {return <FaClock  className='icon-contacto'/>}
-  }
   return (
     <div className='infocontacto-container'>
-        {retonarIconoContacto(props.referencia)}
+        <FaCircle className='icon-contacto'/>
         <p className='parraph-contacto'>{props.texto}</p>
         <p>{props.ref}</p>
     </div>
